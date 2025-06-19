65ef3b4c15a76f7da8d97a7887dfc04d00c5d682
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2025-06-19 16:27:03 -0600

    Simplify some macros

--- source/material_model/rheology/diffusion_creep.cc
+++ source/material_model/rheology/diffusion_creep.cc
@@ -335,14 +335,14 @@ namespace aspect
 {
   namespace MaterialModel
   {
+    namespace Rheology
+    {
 #define INSTANTIATE(dim) \
-  namespace Rheology \
-  { \
-    template class DiffusionCreep<dim>; \
-  }
+  template class DiffusionCreep<dim>;
 
-    ASPECT_INSTANTIATE(INSTANTIATE)
+      ASPECT_INSTANTIATE(INSTANTIATE)
 
 #undef INSTANTIATE
+    }
   }
 }
